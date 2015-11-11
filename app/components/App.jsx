b9d077c0d4ff416a9f35d637f47166a1270c5008
Dan Muzyka <dmuzyka@phase2technology.com>
2015-11-10 17:09:13 -0800

    React and Flux chapter

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,64 +1,76 @@
+import AltContainer from 'alt/AltContainer';
 import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+//import connect from '../decorators/connect';
 
+//@connect(NoteStore)
 export default class App extends React.Component {
+  /*
+  these lines can be removed now
   constructor(props) {
     super(props);
 
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    };
+    this.storeChanged = this.storeChanged.bind(this);
+    this.state = NoteStore.getState();
 
-    this.findNote = this.findNote.bind(this);
-    this.addNote = this.addNote.bind(this);
-    this.editNote = this.editNote.bind(this);
-    this.deleteNote = this.deleteNote.bind(this);
+    //this.findNote = this.findNote.bind(this);
+    //this.addNote = this.addNote.bind(this);
+    //this.editNote = this.editNote.bind(this);
+    //this.deleteNote = this.deleteNote.bind(this);
   }
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+  storeChanged(state) {
+    this.setState(state);
+  }
+  */
 
   render() {
-    const notes = this.state.notes;
+    //const notes = this.props.notes;
 
     return (
       <div>
         <button className='add-note' onClick={this.addNote}>+</button>
-        <Notes notes={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
+        <AltContainer
+          stores={[NoteStore]}
+          inject={ {
+            notes: () => NoteStore.getState().notes
+          } }>
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
       </div>
     );
   }
 
   addNote() {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
-    });
+    NoteActions.create({task: 'New Task'});
+
+    //this.setState({
+    //  notes: this.state.notes.concat([{
+    //    id: uuid.v4(),
+    //    task: 'New task'
+    //  }])
+    //});
   }
 
   editNote(id, task) {
-    let notes = this.state.notes;
-    const noteIndex = this.findNote(id);
-
-    if(noteIndex < 0) {
-      return;
-    }
-
-    notes[noteIndex].task = task;
-    this.setState({notes});
+    NoteActions.update({id, task});
+    //let notes = this.state.notes;
+    //const noteIndex = this.findNote(id);
+    //
+    //if(noteIndex < 0) {
+    //  return;
+    //}
+    //
+    //notes[noteIndex].task = task;
+    //this.setState({notes});
   }
 
   findNote(id) {
@@ -73,15 +85,16 @@ export default class App extends React.Component {
   }
 
   deleteNote(id) {
-    const notes = this.state.notes;
-    const noteIndex = this.findNote(id);
-
-    if(noteIndex < 0) {
-      return;
-    }
-
-    this.setState({
-      notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
-    });
+    NoteActions.delete(id);
+    //const notes = this.state.notes;
+    //const noteIndex = this.findNote(id);
+    //
+    //if(noteIndex < 0) {
+    //  return;
+    //}
+    //
+    //this.setState({
+    //  notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
+    //});
   }
 }
